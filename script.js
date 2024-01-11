f28e9f4ae7f59cc5340f0bbc7acb510cd54652a4
JigoroChop <92116114+JigoroChop@users.noreply.github.com>
2024-01-10 23:25:57 -0800

    Change prompts and add non-case sensitive player input prompt

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 function getComputerChoice(){
-    const RPS = ['Rock', 'Paper', 'Scissors']
-    let compChoice = RPS[Math.floor(Math.random() * 3)];
-    return compChoice
+    const RPS = ['Rock', 'Paper', 'Scissors'];
+    let computerChoice = RPS[Math.floor(Math.random() * 3)];
+    return computerChoice;
 }
+
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection == 'Rock' && computerSelection == 'Paper'){
-        return 'You lose!'
-    }  else if (playerSelection == 'Paper' && computerSelection == 'Scissors'){
-        return 'You lose!'
-    } else if (playerSelection == 'Scissors' && computerSelection != 'Rock') {
-        return 'You lose!'
-    } else if (playerSelection == 'Rock' && computerSelection == 'Rock' || playerSelection == 'Paper' && computerSelection == 'Paper' || playerSelection == 'Scissors' && computerSelection == 'Scissors'){
-        return 'Tie game! Try again.'
-    } else {
-        return 'You win!';
-    }
+        return 'You lose! Paper beats Rock'
+}   else if (playerSelection == 'Paper' && computerSelection == 'Scissors'){
+        return 'You lose! Scissors beats Paper'
+} else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
+    return 'You lose! Rock beats Scissors'
+} else if (playerSelection == 'Rock' && computerSelection == 'Rock' || playerSelection == 'Paper' && computerSelection == 'Paper' || playerSelection == 'Scissors' && computerSelection == 'Scissors'){
+    return 'You both chose ' + playerSelection + '. Tie game! Try again.'
+} else {
+    return 'You chose ' + playerSelection + ' & Computer chose ' + computerSelection + '. You win!';
 }
-
-const playerSelection = 'Rock'
-const computerSelection = getComputerChoice()
-console.log('You chose ' + playerSelection)
-console.log('Computer chose ' + computerSelection)
-console.log(playRound(playerSelection,computerSelection))
+}
+playerChoice = prompt('Choose your weapon');
+let playerSelection = playerChoice.toUpperCase().substr(0,1) + playerChoice.toLowerCase().substr(1);
+let computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
