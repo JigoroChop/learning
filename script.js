956aa1ee934080725496cfbdbf3d20ace7c0c0dd
JigoroChop <92116114+JigoroChop@users.noreply.github.com>
2024-01-11 00:09:07 -0800

    Add score count and set number of rounds to 5 (without using a loop)

--- script.js
+++ script.js
@@ -7,18 +7,39 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection == 'Rock' && computerSelection == 'Paper'){
-        return 'You lose! Paper beats Rock'
+        computerScore++;
+        return 'You lose! Paper beats Rock ';
 }   else if (playerSelection == 'Paper' && computerSelection == 'Scissors'){
-        return 'You lose! Scissors beats Paper'
+        computerScore++;
+        return 'You lose! Scissors beats Paper ';
 } else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
-    return 'You lose! Rock beats Scissors'
+    computerScore++;
+    return 'You lose! Rock beats Scissors ';
 } else if (playerSelection == 'Rock' && computerSelection == 'Rock' || playerSelection == 'Paper' && computerSelection == 'Paper' || playerSelection == 'Scissors' && computerSelection == 'Scissors'){
-    return 'You both chose ' + playerSelection + '. Tie game! Try again.'
+    return 'You both chose ' + playerSelection + '. Tie game! Try again. '
 } else {
-    return 'You chose ' + playerSelection + ' & Computer chose ' + computerSelection + '. You win!';
+    playerScore++;
+    return 'You chose ' + playerSelection + ' & Computer chose ' + computerSelection + '. You win! ';
 }
 }
-playerChoice = prompt('Choose your weapon');
-let playerSelection = playerChoice.toUpperCase().substr(0,1) + playerChoice.toLowerCase().substr(1);
-let computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+let playerScore = 0;
+let computerScore = 0; 
+
+function game(){
+    playerChoice = prompt('Choose your weapon');
+    let playerSelection = playerChoice.toUpperCase().substr(0,1) + playerChoice.toLowerCase().substr(1);
+    let computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+    console.log('You: ' + playerScore + ' - ' + 'Computer: ' + computerScore);
+}
+
+game()
+game()
+game()
+game()
+game()
+
+// Look to see if you can implement a check that pauses the game until a valid PROMPT is entered.
+// e.g. when playerChoice prompt pops up, and you enter "apple soup" -- the function prompts you to enter a valid response (rock, paper, scissors)
+// this should be case-insensitive. Could be good to learn how to check the value of that input against the values in an array.
