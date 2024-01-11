d40f567ae2a183fdd23f8e44f303d6fce2531514
JigoroChop <92116114+JigoroChop@users.noreply.github.com>
2024-01-10 19:25:26 -0800

    Add round of RPS between computer and pre-defined player choice

--- script.js
+++ script.js
@@ -4,4 +4,22 @@ function getComputerChoice(){
     return compChoice
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection){
+    if (playerSelection == 'Rock' && computerSelection == 'Paper'){
+        return 'You lose!'
+    }  else if (playerSelection == 'Paper' && computerSelection == 'Scissors'){
+        return 'You lose!'
+    } else if (playerSelection == 'Scissors' && computerSelection != 'Rock') {
+        return 'You lose!'
+    } else if (playerSelection == 'Rock' && computerSelection == 'Rock' || playerSelection == 'Paper' && computerSelection == 'Paper' || playerSelection == 'Scissors' && computerSelection == 'Scissors'){
+        return 'Tie game! Try again.'
+    } else {
+        return 'You win!';
+    }
+}
+
+const playerSelection = 'Rock'
+const computerSelection = getComputerChoice()
+console.log('You chose ' + playerSelection)
+console.log('Computer chose ' + computerSelection)
+console.log(playRound(playerSelection,computerSelection))
